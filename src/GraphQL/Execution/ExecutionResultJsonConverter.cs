77d86b1e450339e98434b51e8c5ec8f4e0458f28
Aleks <Aleksandar.Manojlovic@hsntech.com>
2018-12-04 18:59:53 -0800

    updating json writer to ignore time zone info

--- src/GraphQL/Execution/ExecutionResultJsonConverter.cs
+++ src/GraphQL/Execution/ExecutionResultJsonConverter.cs
@@ -19,6 +19,8 @@ namespace GraphQL
         {
             if (value is ExecutionResult)
             {
+                writer.DateTimeZoneHandling = DateTimeZoneHandling.Unspecified;
+             
                 var result = (ExecutionResult) value;
 
                 writer.WriteStartObject();
